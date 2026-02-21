eb310909cb366d38f4c9121d2e2926ada3983318
Davse <davserc@gmail.com>
2026-02-20 21:23:22 -0300

    space disk

--- services/vast/gpu_manager/constants.py
+++ services/vast/gpu_manager/constants.py
@@ -5,7 +5,7 @@ DEFAULT_NUM_GPUS = 1
 DEFAULT_MAX_PRICE = 0.50
 DEFAULT_MIN_RELIABILITY = 0.95
 DEFAULT_DOCKER_IMAGE = "davse/flask-yolo-service:latest"
-DEFAULT_DISK_SPACE = 10.0
+DEFAULT_DISK_SPACE = 20.0
 MIN_DISK_SPACE = 10.0
 
 # Common Docker images for ML/AI workloads
